7ddd3d422ea960cfd93c3a5f5bb2c92beb601ec9
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-07-11 08:11:02 -0600

    Add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		\App\Models\User::factory(10)->create();
+
+		$jesus = \App\Models\User::create([
+			'name' => 'Jesus Avelar',
+			'username' => 'admin',
+			'email_verified_at' => now(),
+			'email' => 'admin@admin.com',
+			'password' => bcrypt('123456')
+		]);
+    }
+}
