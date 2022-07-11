eb21688d6dfb26337b5f1ba5433c11d7103a2ac6
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-07-11 08:11:45 -0600

    Add jquery and env.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,10 @@ window._ = require('lodash');
 
 try {
 	window.Popper = require('popper.js').default;
+	window.$ = window.jQuery = require('jquery');
 	require('bootstrap');
+
+	require('./backend/env.js');
 } catch (error) {
 	console.log(error)
 }
