c7804f2c5e9000ac2ce14f9b666856fba81df06a
Jesús Avelar <javelar@hightech-corp.com>
2022-07-11 10:53:01 -0600

    Mejoras en diseño

--- resources/views/backend/env/index.blade.php
+++ resources/views/backend/env/index.blade.php
@@ -2,14 +2,14 @@
 @section('content')
 	<div class="container py-5">
 		<div class="card">
-			<div class="card/body">
-				<table class="table table-bordered">
+			<div class="card-body">
+				<table class="table">
 					<tbody>
 						@foreach($keys as $id => $key)
 							<tr id="{{$id}}">
 								<td>{{$id}}</td>
 								<th>{{$key['value']}}</th>
-								<td><a href="javascript:void(0)" id="editEnv" data-id="{{ $id }}" class="btn btn-info">Editar</a></td>
+								<td><a href="javascript:void(0)" id="editEnv" data-id="{{ $id }}" class="btn btn-primary btn-sm">Editar</a></td>
 							</tr>
 						@endforeach
 					</tbody>
@@ -18,23 +18,23 @@
 		</div>
 	</div>
 
-	<div class="modal fade" id="ajax-crud-modal" aria-hidden="true">
-		<div class="modal-dialog">
+	<div class="modal fade" id="envModal" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h4 class="modal-title" id="postCrudModal"></h4>
+					<h4 class="modal-title" id="envTitleModal"></h4>
 					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div class="modal-body">
 					<form id="envForm" name="envForm" class="form-horizontal">
 
 						<div class="mb-3">
-							<label for="name" class="control-label">Llave</label>
-							<input type="text" class="form-control" id="key" name="key" value="" required="">
+							<label for="name" class="form-label">Llave</label>
+							<input type="text" class="form-control" id="key" name="key" value="" required="" readonly>
 						</div>
 
 						<div class="mb-3">
-							<label class="control-label">Valor</label>
+							<label class="form-label">Valor</label>
 							<input class="form-control" id="value" name="value" value="" required="">
 						</div>
 
