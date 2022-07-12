6e7789bfbb99e521d136b9e6d4d7fcfb0bf3ef26
Jesús Avelar <javelar@hightech-corp.com>
2022-07-11 21:39:35 -0600

    traducciones

--- lang/en/global.php
+++ lang/en/global.php
@@ -4,8 +4,8 @@ return [
 	'or' => 'O',
 	'i_have_an_account' => 'Tengo una cuenta',
 	'google_analytics' => 'ID - Google Analytics',
-	'site_logo' => 'Logotipo del sitio web',
-	'description' => 'Descripción',
+	'site_logo' => 'Website logo',
+	'description' => 'Description',
 	'general' => 'General',
-	'update' => 'Actualizar',
+	'update' => 'Update',
 ];
