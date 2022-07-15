ed0677ce7d1a51bf44c95bcb09c1ea50adc4bc97
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-07-15 08:39:59 -0600

    Se añadieron traducciones a inglès

--- lang/en/global.php
+++ lang/en/global.php
@@ -1,11 +1,36 @@
 <?php
 
 return [
+	//Actions
+	'create' => 'New',
+	'add' => 'Add',
+	'edit' => 'Edit',
+	'update' => 'Update',
+	'delete' => 'Delete',
+
+	// Responses
+	'successfully_added' => 'The registration was successfully added.',
+	'successfully_updated' => 'The registration was successfully updated.',
+	'successfully_destroy' => 'The record was successfully deleted.',
+
+	// Forms
+	'username' => 'Username',
+	'name' => 'Name',
+	'email' => 'E-Mail',
+	'roles' => 'Roles',
+	'permissions' => 'Permissions',
+
 	'or' => 'O',
 	'i_have_an_account' => 'Tengo una cuenta',
 	'google_analytics' => 'ID - Google Analytics',
 	'site_logo' => 'Website logo',
 	'description' => 'Description',
 	'general' => 'General',
-	'update' => 'Update',
+
+	//UI
+	'title' => 'Title',
+	'email_verified_at' => 'Verified mail',
+	'update_at' => 'Last update',
+	'return_back' => 'Go back',
+	
 ];
