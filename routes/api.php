ff76f804171ca8f44e12450cd2106c95e58eadfc
Jesús Avelar <javelar@hightech-corp.com>
2022-07-21 08:17:57 -0600

    Modulo de tareas eliminado

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\TaskController;
 use App\Http\Controllers\API\AuthController;
 
 /*
@@ -19,6 +18,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group( function () {
-	Route::resource('tasks', TaskController::class);
 	Route::post('logout', [AuthController::class, 'logout']);
 });
